13afa3d3313613fdb65551f3a2491eda73804a9e
PawelJa <janecki.paw@gmail.com>
2018-07-12 12:31:28 +0200

    modules

---
+++ modules/models.ts
@@ -0,0 +1,13 @@
+export class One {
+    constructor() {
+        console.log("Class one");
+    }
+}
+
+export class Two {
+    constructor() {
+        console.log("Class two");
+    }
+}
+
+export const PI = 3.14;
