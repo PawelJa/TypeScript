6fa7e9018103a0d5349a5998c94cc43eefcd316c
PawelJa <janecki.paw@gmail.com>
2018-07-12 12:10:15 +0200

    Typical problems with 'this'

---
+++ this_arrowFunctions/thisProblem.ts
@@ -0,0 +1,23 @@
+class ThisProblem{
+
+    msg: string = 'Witaj swiecie';
+
+    printFunction() {
+        return function() {
+            console.log(this.msg);
+        }
+    }
+
+    printFunction2() {
+        return () => {
+            console.log(this.msg)
+        }
+    }
+}
+
+var example = new ThisProblem();
+var fun = example.printFunction();
+fun();
+
+var fun2 = example.printFunction2();
+fun2();
